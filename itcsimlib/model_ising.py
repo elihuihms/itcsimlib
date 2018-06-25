6697d9b1d7fb62be05014e89f7062d11455d3985
Elihu Ihms <mail@elihuihms.com>
2018-06-24 20:07:47 -0400

    Ising model configuration plotter
    Updated and moved model configuration plotter methods to the base Ising class, added new example script.

--- itcsimlib/model_ising.py
+++ itcsimlib/model_ising.py
@@ -226,6 +226,102 @@ class Ising(ITCModel):
 		
 		return ret
 
+	def draw_lattices(self, file, size=1.0, show_energies=True):
+		"""Draw a PDF depicting the energetically-unique configurations of the model, with annotations.
+		
+		Arguments
+		---------
+		file : string
+			The path at which to save the file.
+		size : float
+			The radius (for circular lattices) or width (for linear lattices), in cm of the depictions.
+		
+		Returns
+		-------
+		None
+		
+		Notes
+		-----
+			Uses the current model parameter values to determine energetically degenerate (w.r.t. free energy) configurations.
+		"""
+
+		from pyx import canvas,style,box,path,text,unit,color
+
+		if sum(self.gibbs) == 0:
+			self.set_energies(T0=298.15,T=298.15)
+
+		c = canvas.canvas()
+
+		def _draw_linear_lattice(self,loc,w,n,key=None,energy=None,degeneracy=None):	
+			x,y = loc
+			sx,sy = w,n*w
+
+			x0,x1 = x-(sx/2.0),x+(sx/2.0)
+			y0,y1 = y-(sy/2.0),y+(sy/2.0)
+			rect = box.polygon([(x0, y0),(x1, y0),(x1, y1),(x0, y1)])
+			c.stroke(rect.path(), [style.linewidth.Thick]) # deformer.smoothed(radius=w*2)])
+
+			for i in xrange(0,n):
+				if key[i] > 0:
+					c.fill(path.circle(x,y+(i*w)-(sy/2.0)+(w/2.0),w*0.3), [color.rgb.blue])
+				c.stroke(path.circle(x,y+(i*w)-(sy/2.0)+(w/2.0),w*0.4), [style.linewidth.Thick])
+				
+			if show_energies and energy != None:
+				texrun = text.defaulttexrunner
+				t = texrun.text(x+(w*0.6),y+(0.4*sy), r"%s"%str(energy), [text.halign.boxleft, text.valign.middle] )
+				if unit.tocm(t.width) < 2.0*w:
+					c.insert(t)
+
+			if degeneracy != None:
+				t = c.text(x+(w*0.6),y-(0.4*sy), r"x%s"%str(degeneracy), [text.halign.boxleft, text.valign.top])
+
+		def _draw_circular_lattice(self,loc,r,n,key=None,energy=None,degeneracy=None):
+			x,y = loc
+			chord = 2.0*r*math.sin(math.pi/n)
+			site_r = (r-(chord/2.0))*math.sin(math.pi/n)
+			outer_r = site_r / math.sin(math.pi/n) + site_r
+			inner_r = site_r / math.sin(math.pi/n) - site_r
+
+			c.stroke(path.circle(x,y,outer_r), [style.linewidth.Thick])
+			c.stroke(path.circle(x,y,inner_r), [style.linewidth.Thick])
+
+			if show_energies and energy != None:
+				texrun = text.defaulttexrunner
+				t = texrun.text(x, y, r"%s"%str(energy), [text.halign.boxcenter, text.valign.middle] )
+				if unit.tocm(t.width) < 2.0*inner_r:
+					c.insert(t)
+			
+			if degeneracy != None:
+				t = c.text(x+(r*0.68),y-(r*0.68), r"x%s"%str(degeneracy), [text.halign.boxleft, text.valign.top])
+
+			for i in xrange(0,n):
+				circ_x = x+(math.cos(2.0*math.pi/n*i)*(r-(0.5*chord)))
+				circ_y = y+(math.sin(2.0*math.pi/n*i)*(r-(0.5*chord)))
+				if key[i] > 0:
+					c.fill(path.circle(circ_x,circ_y,site_r*0.6), [color.rgb.blue])
+				c.stroke(path.circle(circ_x,circ_y,site_r*0.8), [style.linewidth.Thick])
+
+		for i in xrange(self.nsites+1):
+			configurations = {} # keyed by energy, (key,weight)
+			for j in xrange(self.nconfigs):
+				if self.bound[j] == i:
+					if self.gibbs[j] in configurations.keys():
+						configurations[ self.gibbs[j] ][1]+=1
+					else:
+						configurations[ self.gibbs[j] ] = [self.configs[j],1]
+			
+			for j,energy in enumerate(sorted(configurations.keys())):
+				if self.circular:
+					_draw_circular_lattice(c,
+						(2.2*size*j,0-2.2*size*i),size,self.nsites,
+						key=configurations[energy][0],energy="%.1f"%(convert_from_J(self.units,energy)),degeneracy=configurations[energy][1])
+				else:
+					_draw_linear_lattice(c,
+						(2.2*size*j,0-1.1*size*i*self.nsites),size,self.nsites,
+						key=configurations[energy][0],energy="%.1f"%(convert_from_J(self.units,energy)),degeneracy=configurations[energy][1])
+					
+		c.writePDFfile(file)
+
 	def set_energies(self,T0,T):
 		"""Set the free and enthalpic energy of each lattice configuration using the current parameter values. This method is a stub that child classes should replace.
 		
@@ -286,7 +382,7 @@ class FullAdditive(Ising):
 						self.enthalpies[i]+=dGb
 						self.config_expressions[i] += self.parameter_symbols['dGb']
 
-					elif self.circular:
+					else:
 						self.gibbs[i]+=dGa
 						self.enthalpies[i]+=dGa
 						self.config_expressions[i] += self.parameter_symbols['dGa']
@@ -294,7 +390,7 @@ class FullAdditive(Ising):
 					if self.get_site_occupancy(i,j-1): # is previous neighboring site occupied?
 						pass # Note: this avoids double counting, and thus is implemented as in Saroff & Kiefer
 
-					elif self.circular:
+					else:
 						self.gibbs[i]+=dGa
 						self.enthalpies[i]+=dGa
 						self.config_expressions[i] += self.parameter_symbols['dGa']
@@ -366,14 +462,14 @@ class NonAdditive(Ising):
 			
 			for j in xrange(self.nsites):
 				if self.get_site_occupancy(i,j): # is site occupied?
-					
+
 					if self.get_site_occupancy(i,j+1): # is the next neighboring site occupied?
 						if self.get_site_occupancy(i,j-1): # is previous neighboring site occupied?
 							self.gibbs[i]+= dGZ
 							self.enthalpies[i]+= dHZ
 							self.config_expressions[i] += self.parameter_symbols['dGZ']
 							
-						elif self.circular:
+						else:
 							self.gibbs[i]+= dGY
 							self.enthalpies[i]+= dHY
 							self.config_expressions[i] += self.parameter_symbols['dGY']
@@ -383,7 +479,7 @@ class NonAdditive(Ising):
 						self.enthalpies[i]+= dHY
 						self.config_expressions[i] += self.parameter_symbols['dGY']
 						
-					elif self.circular:
+					else:
 						self.gibbs[i]+= dGX
 						self.enthalpies[i]+= dHX
 						self.config_expressions[i] += self.parameter_symbols['dGX']
